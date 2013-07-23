0cdcabe2338b222d6a744de2543e2aeb1b5bc1f7
Joe McBride <jmcbride@rackspace.com>
2013-07-23 17:06:35 -0500

    adding cookbooks directory

---
+++ cookbooks/get_cookbooks.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+git clone https://github.com/opscode-cookbooks/apt.git
+git clone https://github.com/opscode-cookbooks/apt.git
+git clone https://github.com/opscode-cookbooks/build-essential.git
+git clone https://github.com/opscode-cookbooks/vim.git
+git clone https://github.com/opscode-cookbooks/python.git
+git clone https://github.com/opscode-cookbooks/rabbitmq.git
+git clone https://github.com/opscode-cookbooks/erlang.git
